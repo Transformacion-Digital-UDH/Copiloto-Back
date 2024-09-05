f0beda0bacc4139b195674e8bc698044ec3454aa
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-09-04 23:50:08 -0500

    route: add route apiresource users

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']); //cierre de sesión
     Route::get('/me', [AuthController::class, 'me']);
 });
+
+Route::apiResource('users', \App\Http\Controllers\UserController::class);
