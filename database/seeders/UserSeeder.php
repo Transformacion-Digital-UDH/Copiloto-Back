d35612069d55362a6c40c98a401728c08c61d590
Joel-IC <joel.ic2000@hotmail.com>
2024-09-06 13:01:53 -0500

    Creacion de Role y dependencias

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data['name'] = "Jonathan";
+        $data['email'] = "jonathan123@gmail.com";
+        $data['role'] = "admin";
+        $data['password'] = bcrypt(123456);
+        \App\Models\User::create($data);
+
+        $data['name'] = "Estudiante";
+        $data['email'] = "estudiante@gmail.com";
+        $data['role'] = "estudiante";
+        $data['password'] = bcrypt(123456);
+        $estudiante = \App\Models\User::create($data);
+
+        $data['name'] = "PAISI";
+        $data['email'] = "paisi@gmail.com";
+        $data['role'] = "paisi";
+        $data['password'] = bcrypt(123456);
+        $coordinador = \App\Models\User::create($data);
+
+        $data['name'] = "Coordinador";
+        $data['email'] = "coordinador@gmail.com";
+        $data['role'] = "coordinador";
+        $data['password'] = bcrypt(123456);
+        $coordinador = \App\Models\User::create($data);
+    }
+}
