523b23bb580049da93e2c943218af61ba4dd995f
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-09-12 23:33:41 -0500

    change name class name

--- database/factories/JuryFactory.php
+++ database/factories/JuryFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Jury>
  */
-class JuryaFactory extends Factory
+class JuryFactory extends Factory
 {
     /**
      * Define the model's default state.
