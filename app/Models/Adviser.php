a5cc4f5aec5c6c066c59e998a4182a1c40786dc7
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-09-16 16:17:58 -0500

    Devolver asesores para seleccionar hecho

---
+++ app/Models/Adviser.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use MongoDB\Laravel\Eloquent\Model;
+
+class Adviser extends Model
+{
+    use HasFactory;
+
+    protected $collection = 'advisers'; // Nombre de la colección en MongoDB
+    protected $fillable = [
+        'adv_name',
+        'adv_lastname_m',
+        'adv_latsname_f',
+        'adv_orcid',
+        'adv_user_id',
+        'adv_is_jury'
+    ];
+
+    // Relación con el usuario
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
