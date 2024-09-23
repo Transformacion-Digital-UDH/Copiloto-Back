79a3a67bdc8cf9078fc250b099ffebd170397b56
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-09-23 09:46:47 -0500

    devolvoendo id en nulo para permitir login

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -15,6 +15,7 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {   
         $role = $this->role->name;
+        $id = null;
 
         if($role == 'estudiante'){
             $id = $this->student->_id;
