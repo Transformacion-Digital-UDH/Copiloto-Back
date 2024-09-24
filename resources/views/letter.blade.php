1ea6941a566684db0c024f3baef710a272da234e
Joel-IC <joel.ic2000@hotmail.com>
2024-09-24 08:56:40 -0500

    Variable sol_num agregada en la vista

--- resources/views/letter.blade.php
+++ resources/views/letter.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Oficio N° 012-ARC-PAISI-FI-UDH</title>
+    <title>Oficio N° {{$solicitude->sol_num}}-ARC-PAISI-FI-UDH</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
     <style>
         @page {
