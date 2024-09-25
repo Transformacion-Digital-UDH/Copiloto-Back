378b44e80363a09ea842ecdabaa90bf75d36ccf6
Joel-IC <joel.ic2000@hotmail.com>
2024-09-25 11:23:59 -0500

    PDF resolucion para designacion de asesor

--- resources/views/resolution_adviser.blade.php
+++ resources/views/resolution_adviser.blade.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Old+Uyghur&display=swap" rel="stylesheet">
     <style>
         @page {
-            size: A4;
+            size: 215.9mm 355.6mm;
             margin: 2mm;
         }
 
@@ -30,7 +30,7 @@
 
         .container-tittle {
             font-weight: bold;
-            font-size: 30px; /* Tamaño del título */
+            font-size: 28px; /* Tamaño del título */
             margin-bottom: 10px;
         }
 
@@ -49,10 +49,6 @@
             text-align: center; /* Centra horizontalmente */
         }
 
-        .fecha{
-            margin-bottom: 15mm;
-        }
-
         .res-date div {
             margin-bottom: 15px; /* Espaciado entre los elementos */
             margin-top: 15px; /* Espaciado entre los elementos */
@@ -65,10 +61,28 @@
             margin-left: 7mm;
         }
 
-        .subtitulo{
-            text-bo
+        .num-res{
+            font-weight: bold;
+            text-decoration: underline;
         }
 
+        .firma {
+        display: flex;
+        justify-content: center; /* Centra horizontalmente */
+        width: 600px;
+        height: 250px; 
+        gap: 10px; /* Espacio entre las imágenes (opcional) */
+        }
+
+        .firma img {
+            max-width: 40%; /* Ajusta el tamaño de las imágenes si es necesario */
+            height: auto;
+        }
+
+        .pie{
+            text-align: left;
+            font-size: 11px;
+        }
     </style>
 </head>
 <body>
@@ -77,15 +91,46 @@
         <div class="container-facultad">Facultad de Ingeniería</div>
     </div>
     <div class="res-date">
-        <div class="">RESOLUCIÓN Nº 1580-2024-D-FI-UDH</div>
-        <div class="fecha">Huánuco, 16 de julio de 2024</div>
+        <p class="num-res">RESOLUCIÓN Nº 1580-2024-D-FI-UDH</p>
+        <p class="fecha">Huánuco, 16 de julio de 2024</p>
         <div class="parrafo">
-            Visto, el Oficio N° 152-2024-CA-PAISI-FI-UDH presentado por el Coordinador
-            del Programa Académico de Ingeniería de Sistemas e Informática y el Expediente N°
-            498351-0000003820, del Bach. <strong>Joel Josue INQUIEL CALDERON</strong>, quién solicita Asesor de
-            Tesis, para desarrollar el trabajo de investigación (Tesis).
+            <p>
+                Visto, el Oficio N° 152-2024-CA-PAISI-FI-UDH presentado por el Coordinador
+                del Programa Académico de Ingeniería de Sistemas e Informática y el Expediente N°
+                498351-0000003820, del Bach. <strong>Joel Josue INQUIEL CALDERON</strong>, quién solicita Asesor de
+                Tesis, para desarrollar el trabajo de investigación (Tesis).
+            </p>
+            <p><strong>CONSIDERANDO:</strong></p>
+            <p>Que, de acuerdo a la Nueva Ley Universitaria 30220, Capítulo V, Art 45º inc.45.2, es procedente su atención, y;</p>
+            <p>
+                Que, según el Expediente N° 498351-0000003820, presentado por el (la) 
+                Bach. <strong>Joel Josue INQUIEL CALDERON</strong>, 
+                quien solicita Asesor de Tesis, para desarrollar su trabajo de investigación, el mismo que propone al Mg. Aldo Enrique Ramirez Chaupis, como Asesor de Tesis, y;
+            </p>
+            <p>Que, según lo dispuesto en el Capítulo II, Art. 27 y 28 del Reglamento General de Grados y Títulos de la Universidad de Huánuco vigente, es procedente atender lo solicitado, y;</p>
+            <p>Estando a Las atribuciones conferidas al Decano de la Facultad de Ingeniería y con cargo a dar cuenta en el próximo Consejo de Facultad.</p>
+            <p><strong>SE RESUELVE:</strong></p>
+            <p><strong style="text-decoration: underline;">Artículo Primero</strong><strong>.-. DESIGNAR,</strong> como Asesor de Tesis del 
+            Bach. <strong>Joel Josue INQUIEL CALDERON</strong>,
+            al Mg. Aldo Enrique Ramirez Chaupis, Docente del Programa Académico de Ingeniería de Sistemas de Informática, Facultad de Ingeniería.</p>
+            <p><strong style="text-decoration: underline;">Artículo Segundo</strong>. - El interesado tendrá un plazo máximo de 6 meses para solicitar revisión del Trabajo de Investigación (Tesis). En todo caso deberá de solicitar nuevamente el trámite con el costo económico vigente.</p>
+            <p style="text-align: center"><br><strong>REGÍSTRESE, COMUNÍQUESE Y ARCHÍVESE</strong></p>
+            <br>
         </div>
-        <div class="sub-titulo">CONSIDERANDO:</div>
+
+        <div class="firma ">
+            <img src="{{ public_path('/img/sello.jpg') }}" alt="Firma 1">
+            <img src="{{ public_path('/img/sello.jpg') }}" alt="Firma 2">
+        </div>
+        <div class="pie">
+            <p style="text-decoration: underline;">Distribución:</p>
+            <p>
+                Fac. de Ingeniería – PAISI – Asesor – Mat. y Reg.Acad. – Interesado – Archivo.<br>
+                <strong>BLCR/EJML/nto.</strong>
+            </p>
+
+
+        </div>
     </div>
 </body>
 </html>
