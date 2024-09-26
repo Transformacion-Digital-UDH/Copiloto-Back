df92126b12ad3768f59b47bf4357fee90748f1e4
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-09-26 14:29:15 -0500

    agregando relacion con oficio y funcion para devolver fecha en formato d-m-y

--- app/Models/DocResolution.php
+++ app/Models/DocResolution.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use MongoDB\Laravel\Eloquent\Model;
+use MongoDB\Laravel\Relations\BelongsTo;
 
 class DocResolution extends Model
 {
@@ -19,4 +20,13 @@ class DocResolution extends Model
         "docres_status",
         "docres_num_res",
     ];
+
+    public function docof(): BelongsTo
+    {
+        return $this->belongsTo(DocOf::class);
+    }
+    
+    public function getCreatedFormattedAttribute(){ 
+        return $this->created_at->format('d-m-Y');
+    }
 }
